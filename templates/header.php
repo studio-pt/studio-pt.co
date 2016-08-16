f72658b028e40a4e6122a29347393b72c0564d56
thirozumi <thirozumi@gmail.com>
2016-08-17 00:48:09 +0900

    wip

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,3 @@
-<header class="banner">
-  <div class="container-fluid">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>">
-      <img src="/assets/images/logo-asyl.svg" alt="<?php bloginfo('name'); ?>">
-    </a>
-  </div>
-</header>
+<a class="brand" href="<?= esc_url(home_url('/')); ?>">
+  <img src="/assets/images/logo-asyl.svg" alt="<?php bloginfo('name'); ?>">
+</a>
