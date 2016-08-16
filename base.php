a624372b6d46b0e8dec0c6674987637b51129829
thirozumi <thirozumi@gmail.com>
2016-08-16 18:16:23 +0900

    wip

--- base.php
+++ base.php
@@ -20,17 +20,17 @@ use Roots\Sage\Wrapper;
       get_template_part('templates/navigation');
     ?>
     <div class="wrap container-fluid" role="document">
-      <div class="content row-fluid">
+      <div class="content row">
         <main class="main">
           <?php include Wrapper\template_path(); ?>
-        </main><!-- /.main -->
+        </main>
         <?php if (Setup\display_sidebar()) : ?>
         <aside class="sidebar">
           <?php include Wrapper\sidebar_path(); ?>
-        </aside><!-- /.sidebar -->
+        </aside>
         <?php endif; ?>
-      </div><!-- /.content -->
-    </div><!-- /.wrap -->
+      </div>
+    </div>
     <?php
       do_action('get_footer');
       get_template_part('templates/footer');
--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,7 @@
-<a class="brand" href="<?= esc_url(home_url('/')); ?>">
-  <img src="/assets/images/logo-asyl.svg" alt="<?php bloginfo('name'); ?>">
-</a>
+<header class="banner">
+  <div class="container-fluid">
+    <a class="brand" href="<?= esc_url(home_url('/')); ?>">
+      <img src="/assets/images/logo-asyl.svg" alt="<?php bloginfo('name'); ?>">
+    </a>
+  </div>
+</header>
