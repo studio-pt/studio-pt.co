839c7bc2a01f4d656d12007bb499dfbccee1978f
thirozumi <thirozumi@gmail.com>
2016-08-17 15:39:13 +0900

    wip

--- front-page.php
+++ front-page.php
@@ -1,24 +1,20 @@
-<?php
-  // check if the flexible content field has rows of data
-  if( have_rows('topic', 'option') ):
-     // loop through the rows of data
-    echo '<div class="topics">';
-    while ( have_rows('topic', 'option') ) : the_row();
-      $id = get_sub_field('image');
-      $size = 'large';
-      $image = wp_get_attachment_image_src( $id, $size );
-      $title = get_sub_field('title');
-      $url = get_sub_field('url');
-      echo '<div class="topic">';
-      echo '<div class="topic-item">';
-      echo '<a class="topic-item-title" href="' . $url . '">' . $title . '</a>';
-      echo '<div class="topic-item-bg" style="background-image:url(' . $image[0] . ')">';
-      echo '</div>';
-      echo '</div>';
-      echo '</div>';
-    endwhile;
-    echo '</div>';
-  else :
-    // no layouts found
-  endif;
-?>
+<?php if (have_rows('topic', 'option')) : ?>
+<div class="topics">
+  <?php while (have_rows('topic', 'option')) : the_row();
+    $id = get_sub_field('image');
+    $size = 'large';
+    $image = wp_get_attachment_image_src( $id, $size );
+    $title = get_sub_field('title');
+    $url = get_sub_field('url');
+  ?>
+  <div class="topic">
+    <div class="card card-vista">
+      <a class="card-foreground" href="<?php echo $url; ?>"><?php echo $title ?></a>
+      <div class="card-background" style="background-image:url(<?php echo $image[0]; ?>)"></div>
+    </div>
+  </div>
+  <?php endwhile; ?>
+</div>
+<?php else: ?>
+    <!-- no layouts found -->
+<?php endif; ?>
