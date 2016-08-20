cadb365e39779f4c251e82bd3ede0bbed71e0080
thirozumi <thirozumi@gmail.com>
2016-08-20 12:29:01 +0900

    wip

--- index.php
+++ index.php
@@ -8,10 +8,8 @@
   <p><?php _e('Sorry, no results were found.', 'sage'); ?></p>
 <?php endif; ?>
 
-<div class="posts-container">
-  <?php while (have_posts()) : the_post(); ?>
-    <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-  <?php endwhile; ?>
-</div>
+<?php while (have_posts()) : the_post(); ?>
+  <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+<?php endwhile; ?>
 
 <?php the_posts_pagination(); ?>
--- templates/content-page.php
+++ templates/content-page.php
@@ -1,6 +1,2 @@
-<div class="page-container">
-<div class="page-item">
-  <?php the_content(); ?>
-  <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
-</div>
-</div>
+<?php the_content(); ?>
+<?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
