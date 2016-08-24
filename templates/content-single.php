89e8847dd1278237a2d962dea056957d89d25268
thirozumi <thirozumi@gmail.com>
2016-08-24 22:21:39 +0900

    wip

--- templates/content-single.php
+++ templates/content-single.php
@@ -11,16 +11,14 @@
     <footer>
       <nav class="navigation posts-navigation">
         <div class="nav-links">
-          <div class="nav-previous"><?php previous_post_link('%link', '前へ'); ?></div>
-          <div class="nav-back">
-            <?php
-              $cat = get_the_category();
-              $cat_id = $cat[0]->cat_ID;
-              $link = get_category_link($cat_id);
-              ?>
-            <a href="<?php echo $link; ?>">もどる</a>
-          </div>
-          <div class="nav-next"><?php next_post_link('%link', '次へ'); ?></div>
+          <?php previous_post_link('%link', '前へ'); ?>
+          <?php
+            $cat = get_the_category();
+            $cat_id = $cat[0]->cat_ID;
+            $link = get_category_link($cat_id);
+            ?>
+          <a href="<?php echo $link; ?>">一覧にもどる</a>
+          <?php next_post_link('%link', '次へ'); ?>
         </div>
       </nav>
       <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
