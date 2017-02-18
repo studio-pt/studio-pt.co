072dfc313c01878616b7946c345e64307a54d6e2
thirozumi <thirozumi@gmail.com>
2017-02-18 13:30:09 +0900

    wip

--- templates/cover.php
+++ templates/cover.php
@@ -1,13 +1,17 @@
 <?php
-  if (get_field('video', 'option') && get_field('poster', 'option')) :
-  $video = get_field('video', 'option');
+  if (get_field('video-sp', 'option') || get_field('video-pc', 'option') && get_field('poster', 'option')) :
+  $video_sp = get_field('video-sp', 'option');
+  $video_pc = get_field('video-pc', 'option');
   $poster = get_field('poster', 'option');
   ?>
   <img class='cover-label cover-trigger' data-href="#b" src="/assets/images/brand__hg.svg" alt="<?php bloginfo('name'); ?>">
   <img class='cover-down cover-trigger' data-href="#b" src="/assets/images/icon__chevron--down.svg" alt="">
   <div class="cover-poster">
-    <video poster="<?= $poster['url'] ?>" muted autoplay loop playsinline>
-      <source src="<?= $video['url'] ?>" type="<?= $video['mime_type'] ?>">
+    <video class="hidden-sm" poster="<?= $poster['url'] ?>" muted autoplay loop playsinline>
+      <source src="<?= $video_pc['url'] ?>" type="<?= $video_pc['mime_type'] ?>">
+    </video>
+    <video class="visible-xs hidden-sm" poster="<?= $poster['url'] ?>" muted autoplay loop playsinline>
+      <source src="<?= $video_sp['url'] ?>" type="<?= $video_sp['mime_type'] ?>">
     </video>
   </div>
 <?php else: ?>
