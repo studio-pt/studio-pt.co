3393e8257df49c01718b4cb35cab63b4fd8985ca
thirozumi <thirozumi@gmail.com>
2017-03-03 14:40:08 +0900

    tinyFixed

--- templates/head.php
+++ templates/head.php
@@ -10,6 +10,8 @@
   <meta name="twitter:card" content="summary_large_image">
 
   <?php
+    $ogd = get_field('og-description', 'option');
+
     if (is_single()) {
       if (have_posts()): while(have_posts()): the_post();
         if (get_the_excerpt()):
@@ -21,16 +23,16 @@
             echo '<meta property="og:description" content="' . $summary . '">'; echo "\n";
             echo '<meta property="twitter:text:description" content="' . $summary . '">'; echo "\n";
           else:
-            echo '<meta property="og:description" content="'; bloginfo('description'); echo '">';echo "\n";
-            echo '<meta property="twitter:text:description" content="'; bloginfo('description'); echo '">';echo "\n";
+            echo '<meta property="og:description" content="' . $ogd . '">';echo "\n";
+            echo '<meta property="twitter:text:description" content="' . $ogd . '">';echo "\n";
           endif;
         endif;
       endwhile; endif;
       echo '<meta property="og:title" content="'; the_title(); echo '">'; echo "\n";
       echo '<meta property="og:url" content="'; the_permalink(); echo '">'; echo "\n";
     } else {
-      echo '<meta property="og:description" content="'; bloginfo('description'); echo '">';echo "\n";
-      echo '<meta property="twitter:text:description" content="'; bloginfo('description'); echo '">';echo "\n";
+      echo '<meta property="og:description" content="' . $ogd . '">';echo "\n";
+      echo '<meta property="twitter:text:description" content="' . $ogd . '">';echo "\n";
       echo '<meta property="og:title" content="'; bloginfo('name'); echo '">'; echo "\n";
       echo '<meta property="og:url" content="'; bloginfo('url'); echo '">'; echo "\n";
     }
